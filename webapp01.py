a87756bc51b1ae2576a6681a0057dbbc02c18491
jumpb0y <116382072+jumpb0y@users.noreply.github.com>
2022-10-21 16:22:17 -0300

    Update webapp01.py

--- webapp01.py
+++ webapp01.py
@@ -3,7 +3,7 @@
 import streamlit as st
 from PIL import Image
 
-image01 = Image.open('desenvolvimento.jpg')
+image01 = Image.open('Bohemian-Rhapsody-1.jpg')
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
 st.title("Bohemian Rhapsody é NOta 1000!")
 
